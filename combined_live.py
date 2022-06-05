e52318d044a98a7e4d615b551a8ba9bae6b1acab
adamwang <adamw43081@gmail.com>
2022-06-04 17:26:13 -0700

    Added comments.

--- combined_live.py
+++ combined_live.py
@@ -5,42 +5,49 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 import img_to_ascii as asc 
 
-
+# ASCII characters to use
 dark_to_light1 = "?$@B\%8&#*oahkbdpqwmzcvunxrjft+~<>i!lI:. "[::-1]
 dark_to_light2 = " .\':;o*O#"
 dark_to_light3 = "@#$\%?*o+~;:\"\'`. "[::-1]
-dark_to_light3 = "@ #.+\'\%:?o*~;$"
+dark_to_light4 = "@ #.+\'\%:?o*~;$"
 light_edges = "|/-\\"
 
-range_mapping = asc.ascii_range_mapping(dark_to_light3)                    
-shading_mapping = asc.ascii_shading_mapping(dark_to_light3)
-edge_mapping = asc.ascii_edge_mapping(light_edges)
 
+scale = 0.15
+# For terminal
+h_to_w_ratio = 2.1
 
 def main():
-    cap = cv2.VideoCapture(0)
+  range_mapping = asc.ascii_range_mapping(dark_to_light4)                    
+  shading_mapping = asc.ascii_shading_mapping(dark_to_light4)
+  edge_mapping = asc.ascii_edge_mapping(light_edges)
 
-    # Check if the webcam is opened correctly
-    if not cap.isOpened():
-        raise IOError("Cannot open webcam")
 
-    _, frame = cap.read()
-    frame = asc.resize_image_to_scale(frame, 0.15*2.1, 0.15, False)
-    x,y,z = frame.shape
-    os.system("mode {},{}".format(y,x))
+  cap = cv2.VideoCapture(0)
 
-    while True:
-        _, frame = cap.read()
-        temp = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("grayscale feed", temp)
-        ascii_frame = asc.img_to_ascii(frame, 0.15, 2.1,range_mapping,shading_mapping,edge_mapping)
-        print(''.join(list(ascii_frame.flatten()))+'\r', end = '' )
+  # Check if the webcam is opened correctly
+  if not cap.isOpened():
+      raise IOError("Cannot open webcam")
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-          break
+  # Take a sample frame and adjust Terminal window accordingly. 
+  _, frame = cap.read()
+  frame = asc.resize_image_to_scale(frame, scale*h_to_w_ratio, scale, False)
+  x,y,z = frame.shape
+  os.system("mode {},{}".format(y,x))
 
-    cap.release()
-    cv2.destroyAllWindows()
+  while True:
+      _, frame = cap.read()
+      temp = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+      # Displays the grayscale video next to the ASCII art
+      cv2.imshow("grayscale feed", temp)
+      ascii_frame = asc.img_to_ascii(frame, scale, h_to_w_ratio, range_mapping,shading_mapping,edge_mapping)
+      print(''.join(list(ascii_frame.flatten()))+'\r', end = '' )
+
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+  cap.release()
+  cv2.destroyAllWindows()
 
 if __name__ ==  "__main__":
     main()
